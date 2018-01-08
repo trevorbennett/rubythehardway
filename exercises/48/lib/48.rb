0e405cf033ad9fea9478bcd6a849bb922015dccd
trevorbennett <garbage@trevorbennett.us>
2018-01-07 22:55:39 -0500

    pass - fail - pass - fail

--- exercises/48/lib/48.rb
+++ exercises/48/lib/48.rb
@@ -1,11 +1,10 @@
-
-class Lexicon
-
-  def scan
-    # getWords = $stdin.gets.chomp
-    # words = getWords.split
+module Lexicon
+  #scan should take in a word (subset string) and return a key value pair
+  def Lexicon.scan(word)
+    if(word == "north")
+      return [["direction",word]]
+    end
   end
-
-
-
+  # getWords = $stdin.gets.chomp
+  # words = getWords.split
 end
--- exercises/48/tests/test_48.rb
+++ exercises/48/tests/test_48.rb
@@ -5,6 +5,12 @@ class TestLexicon < Test::Unit::TestCase
 
   def test_direction_words
     assert_equal(Lexicon.scan("north"), [["direction","north"]])
+    result = Lexicon.scan("south east west")
+    assert_equal(result,[
+      ["direction", "south"],
+      ["direction", "east"],
+      ["direction", "west"]
+      ])
   end
 
 end
